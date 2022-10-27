36f28228c0157e2bd47ea1dd6807bfcd0d7e3aec
ammar zahran <ammar25257@gmail.com>
2022-10-27 09:14:32 +0200

    asdfgh

--- pages/🌸Plant DataBase.py
+++ pages/🌸Plant DataBase.py
@@ -13,3 +13,11 @@ st.set_page_config(
     layout='wide', )
 st.title("Data For Endemic Plants")
 
+# hiding streamlit watermark
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
