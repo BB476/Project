422943ff80b374714379f82cac1e683041e7c50a
Ammar Zahran <btatsmamga@gmail.com>
2022-12-08 09:25:17 +0200

    added soil moisture

--- pages/🕐 Latest Reading.py
+++ pages/🕐 Latest Reading.py
@@ -14,6 +14,8 @@ st.set_page_config(
 
 st.title("Latest data:")
 
+#-------------------------------------------------------------------------------
+
 # fetching data from google sheets
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
@@ -25,65 +27,95 @@ spread = Spread(spreadsheetname, client=client)
 sh = client.open("yarab")
 wks = sh.worksheet("Sheet1")
 
+#---------------------------------------------------------------------------------
+
 # creating a df from sheet
 df = pd.DataFrame(wks.get_all_records())
+dft = df[['Temperature(°C)']].copy()
+dfh = df[['Humidity(%)']].copy()
+dfs = df[['Soil Moisture(PPM)']].copy()
+
+#--------------------------------------------------------------------------
+
+# Viewing last row
 temp_last = df.iloc[-1, 1]
 temp_last2 = str(temp_last)
 Humid_last = df.iloc[-1, 2]
 Humid_last2 = str(Humid_last)
-# Viewing last row
-col11, col22 = st.columns(2)
+soil_last = df.iloc[-1, 3]
+soil_last2 = str(soil_last)
+col11, col22, col33 = st.columns(3)
 col11.metric(label="Temperature", value= temp_last2 + "°C")
 col22.metric(label= "Humidity", value= Humid_last2 + "%")
+col33.metric(label="Soil Moisture", value= soil_last2 + " PPM")
 
 # warning for high temp and humidity
 temp22 = int(temp_last)
 humid22 = int(Humid_last)
 if temp22 > 25 or humid22 > 60:
  st.error("Temperature and Humidty levels might be harmfull to Hypericum Sinaicum")
+ 
+#-------------------------------------------------------------------------------------
    
-# last 3 days average
-average3 = df.tail(3)
-average3.drop(columns=average3.columns[0], axis=1, inplace=True)
-df3 = average3.mean(axis=0)
-# -----
-df32 = df3.drop('Humidity(%)')
-temp_last_3_avg = df32.tail(1)
-temp_last_3_avg = float(temp_last_3_avg)
-temp_last_3_avg = str(temp_last_3_avg)
-# -----
-humid_last_3_avg = df3.tail(1)
-humid_last_3_avg = float(humid_last_3_avg)
-humid_last_7_avg = round(humid_last_3_avg, 1)
-humid_last_3_avg = str(humid_last_3_avg)
+#  last 3 days average temperature
+dft3 = dft.tail(3)
+dft3 = dft3.mean(axis = 0)
+dft3 = dft3.iloc[0]
+dft3 = round(dft3, 1)
+dft3 = str(dft3)
 
-# Viewing last 3 days average
-st.title("Last 3 days average:")
-col111, col222 = st.columns(2)
-col111.metric(label="Temperature", value= temp_last_3_avg + "°C" )
-col222.metric(label= "Humidity", value= humid_last_3_avg + "%")
+# last 3 days average humidity
+dfh3 = dfh.tail(3)
+dfh3 = dfh3.mean(axis = 0)
+dfh3 = dfh3.iloc[0]
+dfh3 = round(dfh3, 1)
+dfh3 = str(dfh3)
 
+# last 3 days average soil moisture
+dfs3 = dfs.tail(3)
+dfs3 = dfs3.mean(axis = 0)
+dfs3 = dfs3.iloc[0]
+dfs3 = round(dfs3, 1)
+dfs3 = str(dfs3)
 
-# last 7 days average
-average3 = df.tail(7)
-average3.drop(columns=average3.columns[0], axis=1, inplace=True)
-df7 = average3.mean(axis=0)
-# -----
-df72 = df3.drop('Humidity(%)')
-temp_last_7_avg = df72.tail(1)
-temp_last_7_avg = float(temp_last_7_avg)
-temp_last_7_avg = str(temp_last_7_avg)
-# -----
-humid_last_7_avg = df7.tail(1)
-humid_last_7_avg = float(humid_last_7_avg)
-humid_last_7_avg = round(humid_last_7_avg, 1)
-humid_last_7_avg = str(humid_last_7_avg)
+#Viewing last 3 days average 
+st.title('Last 3 days average')
+col111, col222, col333 = st.columns(3)
+col111.metric(label="Temperature", value= dft3 + "°C")
+col222.metric(label= "Humidity", value= dfh3 + "%")
+col333.metric(label= "Soil Moisture", value= dfs3 + " PPM")
 
-# Viewing last 7 days average
-st.title("Last 7 days average:")
-col1111, col2222 = st.columns(2)
-col1111.metric(label="Temperature", value= temp_last_7_avg + "°C")
-col2222.metric(label= "Humidity", value= humid_last_7_avg + "%")
+# -----------------------------------------------------------------
+
+#  last 7 days average temperature
+dft7 = dft.tail(7)
+dft7 = dft7.mean(axis = 0)
+dft7 = dft7.iloc[0]
+dft7 = round(dft7, 1)
+dft7 = str(dft7)
+
+# last 7 days average humidity
+dfh7 = dfh.tail(7)
+dfh7 = dfh7.mean(axis = 0)
+dfh7 = dfh7.iloc[0]
+dfh7 = round(dfh7, 1)
+dfh7 = str(dfh7)
+
+# last 7 days average soil moisture
+dfs7 = dfs.tail(7)
+dfs7 = dfs7.mean(axis = 0)
+dfs7 = dfs7.iloc[0]
+dfs7 = round(dfs7, 1)
+dfs7 = str(dfs7)
+
+#Viewing last 7 days average 
+st.title('Last 3 days average')
+col111, col222, col333 = st.columns(3)
+col111.metric(label="Temperature", value= dft3 + "°C")
+col222.metric(label= "Humidity", value= dfh3 + "%")
+col333.metric(label= "Soil Moisture", value= dfs3 + " PPM")
+
+#-----------------------------------------------------------------------------------
 
 # reload button
 col4, col5, col6 = st.columns(3)
